514ef9d3283adaf79c4b1aca846ad815689ccfc0
Brian Clark <bclark8923@gmail.com>
2018-09-17 23:27:19 -0700

    wording

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
   'use strict';
 
   var d = new Date()
-  document.getElementById("footer").innerHTML = document.getElementById("footer").innerHTML + " " + d.getFullYear();
+  document.getElementById("footer").innerHTML = document.getElementById("footer").innerHTML + " " + d.getFullYear() + "&nbsp;&nbsp;|&nbsp;&nbsp;brian@laurenceclark.com";
 
   var years = d.getFullYear() - 2003;
   document.getElementById("yearsof").innerHTML = years + " " + document.getElementById("yearsof").innerHTML;
