1d2eaf98510a7a81141fbe8e677e7b97bb527340
Brian Clark <bclark8923@gmail.com>
2018-09-17 21:42:42 -0700

    wording

--- js/index.js
+++ js/index.js
@@ -5,4 +5,7 @@
   var d = new Date()
   document.getElementById("footer").innerHTML = document.getElementById("footer").innerHTML + " " + d.getFullYear();
 
+  var years = d.getFullYear() - 2003;
+  document.getElementById("yearsof").innerHTML = years + " " + document.getElementById("footer").innerHTML;
+
 })();
